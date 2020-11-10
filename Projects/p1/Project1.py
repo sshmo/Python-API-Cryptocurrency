3029649623c1fc923670bd8b5be4826f5e459edd
sshmo <nabaeazim1@gmail.com>
2020-11-10 17:20:58 +0330

    Added Project1, updated README, requirements

---
+++ Projects/p1/Project1.py
@@ -0,0 +1,104 @@
+import os
+import json
+from requests import Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from datetime import datetime
+import dateutil.parser
+from prettytable import PrettyTable
+from colorama import Fore, Back, Style
+
+convert = 'USD'
+
+url = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+url_end = '?symbol='
+
+
+print()
+print('MY PORTFOLIO')
+print()
+
+portfolio_value = 0.00
+last_updated = 0
+
+table = PrettyTable(['Asset', 'Amount Owned', convert +
+                     ' Value', 'Price', '1h', '24h', '7d'])
+
+with open('portfolio.txt') as inp:
+    for line in inp:
+        symbol, amount = line.split()
+        symbol = symbol.upper()
+
+        symbol_url = url + url_end + symbol
+
+        parameters = {
+            'convert': convert
+            }
+
+        headers = {
+            'Accepts': 'application/json',
+            'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+        }
+
+        session = Session()
+        session.headers.update(headers)
+
+        try:
+            response = session.get(symbol_url, params=parameters)
+            results = response.json()
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            print(e)
+
+        #print(json.dumps(results, sort_keys=True, indent=4))
+
+        currency = results['data'][symbol]
+        rank = currency['cmc_rank']
+        name = currency['name']
+        last_updated = dateutil.parser.isoparse(currency['last_updated'])
+        symbol = currency['symbol']
+
+        quote = currency['quote'][convert]
+        hour_change = quote['percent_change_1h']
+        day_change = quote['percent_change_24h']
+        week_change = quote['percent_change_7d']
+        price = quote['price']
+
+        value = float(price) * float(amount)
+
+        if hour_change > 0:
+            hour_change = Back.GREEN + f'{hour_change:.3f}' + '%' + Style.RESET_ALL
+        else:
+            hour_change = Back.RED + f'{hour_change:.3f}' + '%' + Style.RESET_ALL
+
+        if day_change > 0:
+            day_change = Back.GREEN + f'{day_change:.3f}' + '%' + Style.RESET_ALL
+        else:
+            day_change = Back.RED + f'{day_change:.3f}' + '%' + Style.RESET_ALL
+
+        if week_change > 0:
+            week_change = Back.GREEN + f'{week_change:.3f}' + '%' + Style.RESET_ALL
+        else:
+            week_change = Back.RED + f'{week_change:.3f}' + '%' + Style.RESET_ALL
+
+        portfolio_value += value
+
+        value_string = '{:,}'.format(round(value, 2))
+
+        table.add_row([name + ' (' + symbol + ')',
+                       amount,
+                       '$' + value_string,
+                       '$' + f'{price:.2f}',
+                       str(hour_change),
+                       str(day_change),
+                       str(week_change)])
+
+print(table)
+print()
+
+portfolio_value_string = '{:,}'.format(round(portfolio_value, 2))
+last_updated_string = last_updated.strftime('%B %d, %Y at %I:%M%p')
+
+print('Total Portfolio Value: ' + Back.GREEN +
+      '$' + portfolio_value_string + Style.RESET_ALL)
+print()
+print('API Results Last Updated on ' + last_updated_string)
+print()
--- README.md
+++ README.md
@@ -3,4 +3,46 @@
 (*This project is an updated version of: ['Python-And-Cryptocurrencies-2018](https://github.com/phennaux/Python-and-cryptocurrencies)' created by Philippe Hennaux that was entirely made thanks to an Udemy course created by Ian Annase ['Python & Cryptocurrency: Build 5 Real World Applications-2018](https://www.udemy.com/coinmarketcap/)' The Public API that was used in ['Python & Cryptocurrency: Build 5 Real World Applications](https://www.udemy.com/coinmarketcap/)' was changed to ['Professional API](https://coinmarketcap.com/api/)' in 2018. Since then a lot of changes has occured.*)
 
 
-In this project we use the __*sandbox version*__ with the corresponding sandbox API key to retrieve data from ['coinmarketcap.com](https://coinmarketcap.com)'.
+In this project we use the __*sandbox version*__ with the corresponding sandbox API key to retrieve data from ['sandbox-api.coinmarketcap.com](https://sandbox-api.coinmarketcap.com)'.
+
+The CoinMarketCap API is a suite of high-performance RESTful JSON endpoints. ['See documentation](https://coinmarketcap.com/api/documentation/v1/#)'
+
+This Python project has 5 sub projects using sample cryptocurrency data from ['sandbox-api.coinmarketcap.com](https://sandbox-api.coinmarketcap.com)'.
+
+
+for obtaining live results, replace the url and API key as following:
+
+1 - replace 'sandbox-api.coinmarketcap.com' 
+    with pro-api.coinmarketcap.com
+2 - replace 'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c'
+    with 'X-CMC_PRO_API_KEY': 'your API key'
+
+
+The libraries used in this project are:
+
+- **Prettytables** In order to display data into a table.
+- **Colorama** In order to add colors to a table.
+- **Requests** In order to request files, in this case JSON files.
+- **Datetime** In order to get the date and display it.
+
+With these libraries and the CoinMarketCap API the projects that the course proposed to build are:
+
+1. ***A Cryptocurrency Portfolio App***
+
+    Track all of your crypto assets with ease. See the total value of all your crypto assets combined along with detailed information about each one. Positive and negative values are color coated green and red.
+
+2. ***A Real-Time Price Alert App***
+
+    Get notified when cryptocurrencies hit certain prices in USD. You can keep this program running in the background. Your computer will shout things like, 'Litecoin hit $1200!'.
+
+3. ***A Top 100 Cryptocurreny Ranking App***
+
+    Sort by rank, daily percentage change, or daily volume. Positive and negative values are color coated green and red.
+
+4. ***Predict The Future Values of the Top 100 Cryptocurrencies***
+
+    Explore what the price of cryptocurrencies will be if the global market cap hits certain levels (such as world stock market levels)
+
+5. ***Store Real-Time Information on 1000 Cryptocurrencies in Excel using Python***
+
+    Learn to store cryptocurrency information inside of excel workbooks using Python.
